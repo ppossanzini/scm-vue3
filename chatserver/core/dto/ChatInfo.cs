52deb622cb13ef8eb01c6711dd6448669dcb213c
Paolo Possanzini <paolo@teamdev.it>
2024-06-26 16:25:17 +0200

    gitingnore & dtos

---
+++ chatserver/core/dto/ChatInfo.cs
@@ -0,0 +1,8 @@
+namespace core.dto;
+
+public class ChatInfo
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public DateTime CreationDate { get; set; }
+}
---
+++ chatserver/webapp/dto/ProjectInfo.cs
@@ -0,0 +1,7 @@
+namespace webapp.dto;
+
+public class ProjectInfo
+{
+  public string username { get; set; }
+  public string? projectName { get; set; }
+}
